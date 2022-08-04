8bd92c433a701789fcecc29d7a0cff83dd3bda4a
Huan Tran <huanductran98@gmail.com>
2022-08-03 20:13:37 -0700

    Complete bare_minimum/promisification.js exercises

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,7 +1,7 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('needle');
@@ -29,7 +29,28 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
+
+// var getGitHubProfileAsync = function(user) {
+//   var url = 'https://api.github.com/users/' + user;
+//   var options = {
+//     headers: { 'User-Agent': 'request' },
+//   };
+
+//   return new Promise((resolve, reject) => {
+//     request.get(url, options, function (err, res, body) {
+//       if (err) {
+//         reject(err);
+//       } else if (body.message) {
+//         reject(
+//           new Error('Failed to get GitHub profile: ' + body.message)
+//         );
+//       } else {
+//         resolve(body);
+//       }
+//     });
+//   });
+// };
 
 
 // (2) Asyncronous token generation
@@ -40,25 +61,25 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
-   
+    if (err) { return callback(err, null); }
+
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
       })
       .join('\n');
 
-    callback(funnyFile);
+    callback(null, funnyFile);
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = Promise.promisify(readFileAndMakeItFunny);
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
